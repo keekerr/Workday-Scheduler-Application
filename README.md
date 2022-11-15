938fc49a5b694b4a24dcc85ce24bd3ec61350788
keekerr <keeley.s.sprouse@gmail.com>
2022-11-14 22:58:26 -0500

    Added styling using JS

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # Workday-Scheduler-Application
 
+## Table of Contents
+
+* [Description](#description)
+* [Code Examples](#code-examples)
+* [Important links](#important-links)
+* [Languages Used](#languages-used)
+* [Questions](#questions)
+
+## Description
+
+The purpose of this project was to create a Work Day Scheduler that fit the following Criteria:
+
+- Allows the user to see the current day at the top of the page.
+
+- Allows the user to have access to time bloack for the standard working hours (9:00am-5:00pm).
+
+- Allows the user to see their time blocks color coordinated based on their time status (past, present and future).
+
+- Allows the user to save their daily entries to local storage.
+
+## Code Examples
+Example of Code used to add styling rules to time blocks:
+
+```js
+$(".time-block").each(function(){
+        var currentTime = parseInt(dayjs().format('H'));
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockTime);
+
+        if(blockTime < currentTime) {
+            $(this).addClass("past");
+        } else if(blockTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+```
+
+## Important Links
+[GitHub Repository](https://github.com/keekerr/Workday-Scheduler-Application)
+
+[Deployed Application](https://keekerr.github.io/Workday-Scheduler-Application/)
+
+![Image Example of Deployed Application](https://github.com/keekerr/Workday-Scheduler-Application/blob/main/assets/Work%20Day%20Scheduler%20Example.PNG?raw=true)
+
+## Languages Used
+
+![HTML Badge](https://th.bing.com/th/id/OIP._Ik4_2kbAUkc8WfirxFSLwHaHa?w=100&h=120&c=7&r=0&o=5&pid=1.7)
+![CSS Badge](https://th.bing.com/th/id/OIP.bVCzXbidOak-TcOhmW0QTAHaHa?pid=ImgDet&w=100&h=120&c=7)
+![JavaScript Badge](https://www.simpleimageresizer.com/_uploads/photos/8e50d6a8/java_icon_1_100x120.png)
+## Questions
+
+If you have any questions regarding this project, please feel free to contact me at this email: keeley.s.sprouse@gmail.com
+
+Other examples of projects I have worked on can be viewed on Github via this link: [keekerr](https://github.com/keekerr)
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    // document.getElementById("myBtn").addEventListener("click", displayDate);
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-
-    // var currentHour = "enter info"
-    // var (transform all the ids into an array? name array row ID)
-
+    
     // Add funtion that allows for current date to be displayed at the top of the page everytime the function is executed
     $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY')); 
-    // Add time blocks for standard working hours (see HTML)
-    // Color code each block based on current time (gray for past time bloack, red for current time blocks and green for future time blocks *use for loop*)
-    var timeStyle = function() {
-        var currentTime = moment().format('H');
-        var timeColorStyle = $(".textarea");
-        for (var i = 0; i < timeColorStyle.length; i++){
-            var timeIds = timeColorStyle[i].id;
-            var getTimeIds = document.getElementById(timeColorStyle[i].id);
 
-            if (timeIds < currentTime) {
-                $(getTimeIds).addClass("past");
-            } else if (timeIds > currentTime) {
-                $(getTimeIds).addClass("future");
-            } else {
-                $(getTimeIds).addClass("present");
-            }
+    // 
+    
+    $(".time-block").each(function(){
+        var currentTime = parseInt(dayjs().format('H'));
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockTime);
+
+        if(blockTime < currentTime) {
+            $(this).addClass("past");
+        } else if(blockTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
         }
-    }
-    // Add local storage functionality so that events that are entered/saved in the timeblock remain in the time blocks once the page is refreshed.   
-        
-     
+    })
+  
+    var ids = $("[textarea]");
+    console.log(ids);
+
+    localStorage.setItem("ids", JSON.stringify(ids));
+
 });
     
 
