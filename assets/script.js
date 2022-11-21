0b02940b02d9d6ebe6de2fd3cd1b9575ac5585b8
keekerr <keeley.s.sprouse@gmail.com>
2022-11-21 17:57:53 -0500

    Debugged Local Storage Function

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,17 @@ $(function () {
     // Add funtion that allows for current date to be displayed at the top of the page everytime the function is executed
     $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY')); 
 
-    // 
-    
+    // when button is clicked, send description content to local storage
+    $(".saveBtn").on("click",function(){
+        var time = $(this).parent().attr("id")
+        var value = $(this).siblings(".description").val()
+        localStorage.setItem(time,value)
+    })
+
+   
+   function timeChecker() {
+    var currentTime = parseInt(dayjs().format('H'));
     $(".time-block").each(function(){
-        var currentTime = parseInt(dayjs().format('H'));
         var blockTime = parseInt($(this).attr("id").split("-")[1]);
         console.log(blockTime);
 
@@ -25,11 +32,20 @@ $(function () {
             $(this).addClass("future");
         }
     })
-  
-    var ids = $("[textarea]");
-    console.log(ids);
+   }
+   timeChecker()
+  setInterval(timeChecker,30000)
+    
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
-    localStorage.setItem("ids", JSON.stringify(ids));
 
 });
     
